bb511cd8e0e6ab26470c79bf20425133f422e712
slqpez <santiago.lopez16@udea.edu.co>
2019-07-07 18:06:23 -0500

    Agregando P3fun

---
+++ P3fun.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main()
+{
+    int x,m;
+    
+  	int factorial (int x){
+        float fact;
+         for (int b = x; b > 1; b--){
+             fact = fact * b;
+         }
+        return fact; 
+    }
+    
+    double serie (int x, int m){
+        
+        double resultado=1;
+        
+        int aux=2;
+        for (int i=2;i<=m;i++){
+            
+            if(i%2==0){
+                resultado = resultado - (double)((pow(x,aux))/factorial(aux));
+               
+                
+            }else{
+                resultado = resultado + (double)((pow(x,aux))/factorial(aux));
+                
+                
+            }
+
+            aux =aux +2;
+
+           
+        }
+        printf("El resultado de la serie es: %f\n", resultado); 
+        return resultado;
+           
+    }
+    printf ("Ingrese el valor de X: "); //Leemos el número 
+    scanf("%d", &x);
+    printf ("Ingrese la cantidad de terminos : "); //Leemos el número 
+    scanf("%d", &m);
+    
+    serie(x,m);
+}
